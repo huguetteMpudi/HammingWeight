5130498cb796210f7012adf72b214674156415e2
Huguette Mpudi kipasa <huguettemkipasa@gmail.com>
2022-12-01 19:07:19 -0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+ var n = "000000000000000000000000000010111";
+ var hammingWeight = function(n) {
+
+    const v = (n >>> 1).toString(2);
+
+   
+    
+    console.log(v);
+
+
+};
+hammingWeight(n);
